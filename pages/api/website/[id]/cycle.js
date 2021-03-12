4e7d6a774ae295fdb31e75e19a9b099f9631c835
solomongg <solomongg@126.com>
2021-03-12 09:06:21 +0800

    update by gsy

--- pages/api/website/[id]/cycle.js
+++ pages/api/website/[id]/cycle.js
@@ -1,24 +1,19 @@
 import { getPageviewCycle } from 'lib/queries';
-import { ok, badRequest, methodNotAllowed, unauthorized } from 'lib/response';
-import { DOMAIN_REGEX } from 'lib/constants';
-import { allowQuery } from 'lib/auth';
+import { ok, methodNotAllowed } from 'lib/response';
 
 export default async (req, res) => {
   if (req.method === 'GET') {
-    if (!(await allowQuery(req))) {
-      return unauthorized(res);
-    }
+    const { id, start_at, end_at, url, username } = req.query;
 
-    const { id, start_at, end_at, domain, url, username } = req.query;
+    console.log(id);
 
-    if (domain && !DOMAIN_REGEX.test(domain)) {
-      return badRequest(res);
-    }
+    console.log(username);
 
     const websiteId = +id;
     const startDate = new Date(+start_at);
     const endDate = new Date(+end_at);
-
+    console.log(startDate);
+    console.log(endDate);
     const data = await getPageviewCycle(websiteId, startDate, endDate, url, username);
 
     return ok(res, data);
